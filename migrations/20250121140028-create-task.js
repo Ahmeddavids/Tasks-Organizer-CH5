f2c06662cf0e4d08b503c8e9b511259ad31e82dd
ahmed isiaka <ahmeddavids6@gmail.com>
2025-01-28 13:25:37 +0100

    Updated the migration file

--- migrations/20250121140028-create-task.js
+++ migrations/20250121140028-create-task.js
@@ -17,7 +17,7 @@ module.exports = {
         allowNull: false
       },
       dueDate: {
-        type: Sequelize.DATE,
+        type: Sequelize.STRING,
         allowNull: false
       },
       completed: {
