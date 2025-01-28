05cb8945b9f7cfde39b73a158d61795fd88d7feb
ahmed isiaka <ahmeddavids6@gmail.com>
2025-01-28 12:56:48 +0100

    Added a create task function

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -2,14 +2,24 @@ const taskModel = require('../models/task');
 
 exports.createTask = async (req, res) => {
     try {
-        const {taskName, dueDate, priority, } = req.body;
+        const {taskName, dueDate = new Date(), priority, } = req.body;
         if (!taskName || !dueDate || !priority) {
             return res.status(400).json({
                 message: 'Please enter all fields'
             });
         }
 
-        
+        const newTask = await taskModel.create({
+            taskName,
+            priority,
+            dueDate: dueDate.toLocaleString()
+        });
+
+        // Send a success response
+        res.status(201).json({
+            message: 'Task created successfully',
+            data: newTask
+        })
 
     } catch (error) {
         res.status(500).json({
