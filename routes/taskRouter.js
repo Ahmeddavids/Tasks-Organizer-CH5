788159e73072e550899235939215fda50380a22c
ahmed isiaka <ahmeddavids6@gmail.com>
2025-01-28 14:13:30 +0100

    Added a task update route

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { createTask, completeTask } = require('../controllers/taskController');
+const { createTask, completeTask, updateTask } = require('../controllers/taskController');
 
 const router = express.Router();
-
+// Create Task
 router.post('/task', createTask);
-
+// Complete a Task
 router.put('/task/:id', completeTask);
+// Update Task
+router.patch('/task/:id', updateTask);
 
 module.exports = router;
